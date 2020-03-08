980fb0217511ac625e11c536882645bba4956501
Bhuman Soni <bhuman@yourdiagrams.com>
2020-03-08 17:13:08 +1100

    started adding code to make API calls

--- src/index.js
+++ src/index.js
@@ -11,19 +11,43 @@ class AddressForm extends React.Component {
       suburb: '',
       state: ''
     };
+    this.postCodeRef = React.createRef();
+    this.suburbRef = React.createRef();
+    this.stateRef = React.createRef();
   }
   submitHandler = (event) => {
     event.preventDefault();
-    console.log(event.target.value);
     let postcode = event.target.postcode.value;
     let cState = event.target.cState.value;
     let suburb = event.target.suburb.value;
-    console.log(suburb);
-    console.log(cState);
-    console.log(postcode);
-    console.log(event);
+    if(suburb.length == 0) {
+      alert("No suburb? Please enter a suburb");
+      this.suburbRef.current.focus();
+      return;
+    }
+    if(!Number(postcode)) {
+      alert("Invalid postcode");
+      this.postCodeRef.current.focus();
+      return;
+    }
+    if(cState.length == 0) {
+      alert("No Sate? You must enter a state");
+      this.stateRef.current.focus();
+      return;
+    }
     //make an API call and do some validation 
     //based on responses
+    //TODO: - incomplete
+    const API_KEY = "872608e3-4530-4c6a-a369-052accb03ca8";
+    const BASE_PO_URL="https://digitalapi.auspost.com.au/postcode/";
+    let targetUrl = BASE_PO_URL + `search?q=${suburb}&state=${cState}`;
+      
+    fetch(targetUrl, { 
+      method: 'post', 
+      headers: new Headers({
+       'AUTH-KEY': API_KEY,
+     }), 
+    });
   }
   render() {
     return (
@@ -32,15 +56,21 @@ class AddressForm extends React.Component {
       <p> Please enter the suburb, post code for the state you want to find</p>
       <div className="data">
          <p> Suburb </p>
-        <input type="text" name='suburb'/>
+        <input type="text" 
+        name='suburb'
+        ref={this.suburbRef}  />
       </div>
       <div className="data">
         <p> Post code </p>
-        <input type="text" name='postcode'  />
+        <input type="text" 
+        name='postcode'
+        ref={this.postCodeRef}  />
       </div>
       <div className="data">
         <p> State </p>
-        <input type="text" name='cState'/>
+        <input type="text" 
+        name='cState'
+        ref={this.stateRef}  />
       </div>
       <div className="center"> 
         <input className="submit" type="submit" value="VALIDATE ADDRESS" />
