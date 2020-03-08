4b617a0ae0a49dd0ed1cd6369f11619f34528144
Bhuman Soni <bhuman@yourdiagrams.com>
2020-03-08 14:22:19 +1100

    Initial commit. Added early stage UI without any styling or code to call external API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class AddressForm extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      postcode: 0,
+      suburb: '',
+      state: ''
+    };
+  }
+  submitHandler = (event) => {
+    event.preventDefault();
+    console.log(event.target.value);
+    let postcode = event.target.postcode.value;
+    let cState = event.target.cState.value;
+    let suburb = event.target.suburb.value;
+    console.log(suburb);
+    console.log(cState);
+    console.log(postcode);
+    console.log(event);
+    //make an API call and do some validation 
+    //based on responses
+  }
+  render() {
+    return (
+      <form onSubmit={this.submitHandler}>
+      <h1> Hi there!!! </h1>
+      <p> Please enter the suburt, post code for the state you wish to find</p>
+      <p> Suburb </p>
+      <input type="text" name='suburb'/>
+      <p> Post code </p>
+      <input type="text" name='postcode'  />
+      <p> State </p>
+      <input type="text" name='cState'/>
+      <div> 
+        <input type="submit" value="Validate address " />
+      </div>
+      </form>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// ========================================
+ReactDOM.render(
+  <AddressForm />,
+  document.getElementById('root')
+);
