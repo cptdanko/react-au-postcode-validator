3075815508bc7633911a7eeba99c74e6b999515f
Bhuman Soni <bhuman.soni@gmail.com>
2020-03-09 12:28:26 +1100

    almost complete. The only thing left to add is a state validator

--- src/index.js
+++ src/index.js
@@ -15,33 +15,60 @@ class AddressForm extends React.Component {
     this.suburbRef = React.createRef();
     this.stateRef = React.createRef();
   }
+  validateObject = (pobox, cState, suburb, apiObj) => {
+    if(apiObj.location.toLowerCase() === suburb
+        && apiObj.postcode === pobox
+        && apiObj.state.toLowerCase() === cState) {
+        alert(`Great!!! The post code: ${pobox} is correct for ${suburb} and it's in the state: ${cState.toUpperCase()} `);
+    } else if(apiObj.location.toLowerCase() === suburb &&
+              apiObj.postcode != pobox) {
+        alert(`This  ${pobox} is not the postcode for the suburb: ${suburb}`);
+    } else if(apiObj.location.toLowerCase() === suburb 
+        && apiObj.state.toLowerCase() !== cState) {
+        alert(`The suburb (${suburb}) is not part of the state: ${cState}`);
+    }
+  }
   submitHandler = (event) => {
     event.preventDefault();
     let postcode = event.target.postcode.value;
-    let cState = event.target.cState.value;
-    let suburb = event.target.suburb.value;
+    let cState = event.target.cState.value.toLowerCase();
+    let suburb = event.target.suburb.value.toLowerCase();
     if(suburb.length == 0) {
       alert("No suburb? Please enter a suburb");
       this.suburbRef.current.focus();
       return;
     }
-    if(!Number(postcode)) {
+    postcode = Number(postcode);
+    if(!postcode) {
       alert("Invalid postcode");
       this.postCodeRef.current.focus();
       return;
     }
+    
     if(cState.length == 0) {
       alert("No Sate? You must enter a state");
       this.stateRef.current.focus();
       return;
     }
     let endpoint = `http://localhost:9999/api?q=${suburb}&state=${cState}`;
-    
     fetch(endpoint).then(data => {
       data.json().then(resolved => {
-        console.log(resolved.localities);
-        console.log(resolved.localities.locality);
-        console.log(resolved);
+        if(resolved) {
+          if(resolved.localities.locality) {
+            let localityObjs = resolved.localities.locality;
+            debugger
+            if(Array.isArray(localityObjs)) {
+              for(let i=0; i < localityObjs.length; i++) {
+                this.validateObject(postcode, cState, suburb, localityObjs[i]);
+              }
+            } else {
+              console.log("response is object");
+              this.validateObject(postcode, cState, suburb, localityObjs);
+            }
+          }
+        }
+      }).catch(err => {
+        console.log(err);
       });
      })
   }
